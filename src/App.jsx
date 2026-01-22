8a92f2beff62ff44e01ad879146a36093b593c78
romiiali <ra7ma.anwar@hotmail.com>
2026-01-22 15:16:56 +0200

    adding dependencies

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,26 @@
-import './App.css'
-
+import { FormControl , InputGroup , Container , Button  } from "react-bootstrap";
+// App.jsx
+const clientId = import.meta.env.VITE_CLIENT_ID;
+const clientSecret = import.meta.env.VITE_CLIENT_SECRET;
 function App() {
   
 
   return (
-   <>
-      <h1>Codédex Project Template</h1>
-    </>
+   <Container>
+    <InputGroup>
+        <FormControl onKeyDown={(event)=>{
+          if(event.key==="Enter"){
+            search();
+          }
+        }}/>
+        <Button onClick={search}></Button>
+    </InputGroup>
+   </Container>
   )
+}
+
+function search(){
+
 }
 
 
